d4152058759c245dc48a6bd51edc0bd7b71e82c8
praveen-9014 <151435506+praveen-9014@users.noreply.github.com>
2024-05-20 15:08:02 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,8 @@
 #write a program to find the sum of digits of a given number'
+num = int(input("enter the number"))
+copy = num
+sum = 0
+while(copy!=0):
+    sum +=int(copy%10)
+    copy/=10
+print(f"the sum of digit of {num} is: ",sum)
