4d226ed6b02efa55941499d36c68b5a93575c89d
praveen-9014 <151435506+praveen-9014@users.noreply.github.com>
2024-05-20 14:55:11 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,6 @@
 #write a program to find the factorial of a nummber
+num = int(input("Enter the number: "))
+fact = 1  
+for i in range(1, num + 1):
+    fact *= i 
+print(f"The Factorial of {num} is ",fact)
